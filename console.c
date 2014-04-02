91c613dc39a586af10c7f1e264c95a55f2bf8817
kramekb <>
2014-04-02 16:39:52 +0300

    Add support for left and right arrow keys in console

--- console.c
+++ console.c
@@ -157,6 +157,25 @@ cgaputc(int c)
   crt[pos] = ' ' | 0x0700;
 }
 
+static void
+movecur(int delta)
+{
+  int pos;
+
+  // Cursor position: col + 80*row.
+  outb(CRTPORT, 14);
+  pos = inb(CRTPORT+1) << 8;
+  outb(CRTPORT, 15);
+  pos |= inb(CRTPORT+1);
+
+  pos += delta;
+
+  outb(CRTPORT, 14);
+  outb(CRTPORT+1, pos>>8);
+  outb(CRTPORT, 15);
+  outb(CRTPORT+1, pos);
+}
+
 void
 consputc(int c)
 {
@@ -180,14 +199,18 @@ struct {
   uint r;  // Read index
   uint w;  // Write index
   uint e;  // Edit index
+  int o; // Insert offset
 } input;
 
 #define C(x)  ((x)-'@')  // Control-x
+#define KEY_LEFT 228
+#define KEY_RIGHT 229
 
 void
 consoleintr(int (*getc)(void))
 {
   int c;
+  int i;
 
   acquire(&input.lock);
   while((c = getc()) >= 0){
@@ -196,6 +219,8 @@ consoleintr(int (*getc)(void))
       procdump();
       break;
     case C('U'):  // Kill line.
+      movecur(input.o);
+      input.o = 0;
       while(input.e != input.w &&
             input.buf[(input.e-1) % INPUT_BUF] != '\n'){
         input.e--;
@@ -203,16 +228,50 @@ consoleintr(int (*getc)(void))
       }
       break;
     case C('H'): case '\x7f':  // Backspace
-      if(input.e != input.w){
-        input.e--;
+      if(input.e - input.o != input.w){
+        for(i = 0; i < input.o; ++i) {
+          input.buf[(input.e-input.o-1+i) % INPUT_BUF] = input.buf[(input.e-input.o+i) % INPUT_BUF];
+        }
+        movecur(input.o);
         consputc(BACKSPACE);
+        movecur(-input.o);
+        input.e--;
+        for(i = 0; i < input.o; ++i) {
+          consputc(input.buf[(input.e-input.o+i) % INPUT_BUF]);
+        }
+        movecur(-input.o);
+      }
+      break;
+    case KEY_LEFT:
+      if (input.o < input.e - input.w) {
+        input.o++;
+        uartputc('\b');
+        movecur(-1);
+      }
+      break;
+    case KEY_RIGHT:
+      if (input.o > 0) {
+        input.o--;
+        movecur(1);
       }
       break;
     default:
       if(c != 0 && input.e-input.r < INPUT_BUF){
         c = (c == '\r') ? '\n' : c;
-        input.buf[input.e++ % INPUT_BUF] = c;
+        if(c == '\n' || c == C('D') || input.e == input.r+INPUT_BUF){
+          movecur(input.o);
+          input.o = 0;
+        }
+        for(i = 0; i < input.o; ++i) {
+          input.buf[(input.e-i) % INPUT_BUF] = input.buf[(input.e-i-1) % INPUT_BUF];
+        }
+        input.buf[(input.e-input.o) % INPUT_BUF] = c;
+        input.e++;
         consputc(c);
+        for(i = 0; i < input.o; ++i) {
+          consputc(input.buf[(input.e-input.o+i) % INPUT_BUF]);
+        }
+        movecur(-input.o);
         if(c == '\n' || c == C('D') || input.e == input.r+INPUT_BUF){
           input.w = input.e;
           wakeup(&input.r);
